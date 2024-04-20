28e9929fcd9f461e6ead1e2f65909d69462cc4d4
Tokumoto <takuya.tokumoto@ariseanalytics.com>
2024-04-20 11:39:18 +0900

    20240420

--- README.md
+++ README.md
@@ -1,42 +1,12 @@
-# 異常検知データセットでmetric learningを試す
+# 複数時系列データ × Transformerの実装と評価
 
-- [qiitaでの紹介記事](https://qiita.com/takuya66520126/private/ddb271b57b098dcfe81b)
+- [参照記事](https://zenn.dev/shungo_a/articles/1ae7e8c68b1cbb)
 
 ## セットアップ
-### dockerをインストール
-#### docker desktopをインストールする場合
-- [ここ](https://docs.docker.com/desktop/)などを参考にdocker desktopをインストール
+### xx
 
-#### CLIでインストールする場合
-- ec2でubuntuインスタンスを立ち上げた場合などを想定
-```shell
-sudo apt-get update
-sudo apt-get install docker.io
-sudo gpasswd -a {ユーザ名:ubuntu} docker
-docker --version #表示されればセットアップ完了
-exit #gpasswdでユーザを付与した後に再ログイン必要
-```
 
-### git clone
-```shell
-git clone https://github.com/takuya-tokumoto/deep-metric-fraud-detector.git
-cd deep-metric-fraud-detector
-```
-
-### dorkerの起動して環境構築
-```shell
-./build.sh
-./run.sh #windows環境の場合は run_for_win.sh を実行
-```
-
-### jupyterlabへアクセス
-- ローカルPCで立ち上げた場合はブラウザから`locathost:8888`
-- EC2の場合はブラウザから`{パブリック IPv4 DNS}:8888`
-
-### データの準備
-- `./tmp/`配下に[zipファイル](https://adfi.jp/download/)を格納
-- `sh unzip_files.sh`
-
-### 備考
-- 実行環境
-  - EC2(AMI:Deep Learning Base GPU AMI, instancetype:d4dn.2xlarge, EBS:150GB)
+## iTransformerの特徴
+### 既存手法では各時刻の値でトークン化していたが時系列データ全体をトークン化するように変更
+- 時系列データ全体で考えた時に本来あるはずだった相関を上手く捉えることができない状態で学習がなされていたが、iTransformerではこの課題を解決するために、時系列データ全体をトークン化することで、精度を向上させています。[参考画像1](https://storage.googleapis.com/zenn-user-upload/35625fcb1926-20231106.png)  
+- また、クエリとキーで類似性を算出する際に、既存手法では時系列データに対する相関を組み込めていなかったが、時系列データ全体をトークン化することで変数間の相関を出力できるようになった。[参考画像2](https://storage.googleapis.com/zenn-user-upload/d8b9a1d88dcd-20231107.png)
---
+++ tools/dataset.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+import time
+
+class CreateTimeSeriesData:
+    def __init__(self, target_cols: list) -> None:
+        self.target_cols = target_cols
+        self.dir_order_data = '../data/FEH_00100401_240415220642.csv'
+        self.dir_stock_data = '../data/TimeSeriesResult_20240415215057946.csv'
+        self.month_lists = list()
+        self.months = list()
+        self.base_df = pd.DataFrame()
+
+    def load_data(self) -> pd.DataFrame:
+        """対象データの読み込み"""        
+
+        #機械受注長期時系列
+        order_data = pd.read_csv(self.dir_order_data, skiprows = 9, encoding='shift-jis') #2005年4月～2024年2月
+        order_data = order_data.sort_values('時間軸(月次) コード')
+        # numeric列は'xx,xxx'などのstr型格納されているのでfloat処理
+        numeric_cols = order_data.columns[7:]
+        # すべての列に対してカンマを取り除き、float型に変換
+        for column in numeric_cols:
+            order_data[column] = order_data[column].str.replace(',', '').astype(float)
+
+        # 日経平均
+        stock_data = pd.read_csv(self.dir_stock_data)[63:-1] #2005年4月～2024年2月
+        # stock_data = stock_data.sort_values('時点')
+
+        return order_data, stock_data
+    
+    def get_month_list(self, stock_data):
+        """計算対象となるmonthのリストを取得"""
+        
+        self.month_lists = list(stock_data['時点'])
+        self.months = self.month_lists[1:] # 階差を取得する関係で2005年5月以降の値を取得
+
+    def rename_column(self, order_data: pd.DataFrame, stock_data: pd.DataFrame) -> pd.DataFrame:
+        """加工対象のカラムを名前変更"""
+
+        order_rename_dict = {
+            '産業機械_産業用ロボット': 'Industrial_robots',
+            '産業機械_風水力機械': 'Pneumatic_and_hydraulic_equipment',
+            '産業機械_運搬機械': 'materialshandling_machinery',
+            '産業機械_金属加工機械': 'Metal_working_machinery',
+            '産業機械_冷凍機械': 'Refrigerating_machines',
+            '産業機械_合成樹脂加工機械': 'Plastics_pocessing_machinery'
+        }
+        order_data = order_data.rename(columns=order_rename_dict)
+
+        stock_data = stock_data.rename(columns={'日経平均株価【円】': 'stock'})
+
+        return order_data, stock_data
+
+    def logarithmic_diff_target(self, df: pd.DataFrame, tg_col: str) -> pd.DataFrame:
+        """ターゲット指標に対しlog10で対数変換をした後に階差を取得"""
+        
+        df[tg_col] = np.log10(df[tg_col].astype(float)).diff()
+
+        return df
+    
+    def logarithmic_target(self, df: pd.DataFrame, tg_col: str) -> pd.DataFrame:
+        """ターゲット指標に対しlog10で対数変換のみ処理"""
+        
+        df[tg_col] = np.log10(df[tg_col].astype(float))
+
+        return df
+
+    def concat_dfs(self, order_data: pd.DataFrame, stock_data: pd.DataFrame) -> pd.DataFrame:
+        """stock情報をLeft JOIN"""
+
+        df = pd.merge(order_data, stock_data, left_on='時間軸(月次)', right_on='時点',how='left')
+
+        return df
+
+    def transposition_table(self, df: pd.DataFrame) -> pd.DataFrame:
+        """マート用にテーブルを転置"""
+
+        # マート作成に用いるカラムのみ抽出
+        use_cols = ['時間軸(月次)'] + self.target_cols
+        df = df[use_cols]
+        # 転置させてマートを作成
+        df_t = df[use_cols].set_index('時間軸(月次)').rename_axis(None).T
+
+        return df_t
+
+    def create_base_df(self):
+        """加工用のベースマートを作成"""
+        order_data, stock_data = self.load_data()
+        self.get_month_list(stock_data)
+        order_data, stock_data = self.rename_column(order_data, stock_data)
+        self.base_df = self.concat_dfs(order_data, stock_data)
+
+    def create_original_df_pipeline(self) -> pd.DataFrame:
+        """加工したデータフレーム（df_original）の取り出し"""   
+
+        df = self.base_df.copy() # ベースマートを呼び出し
+        for tg_col in self.target_cols:
+            df = self.logarithmic_diff_target(df, tg_col)
+        df = self.transposition_table(df)
+        df = df[self.months]
+
+        return df    
+    
+    def create_log_df_pipeline(self) -> pd.DataFrame:
+        """加工したデータフレーム（df_log）の取り出し"""   
+
+        df = self.base_df.copy() # ベースマートを呼び出し
+        for tg_col in self.target_cols:
+            df = self.logarithmic_target(df, tg_col)
+        df = self.transposition_table(df)
+        df = df[self.months]
+
+        return df    
